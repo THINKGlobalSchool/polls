22bc331db98165afb2b43345e3c4ad4dffc65f60
Jeff Tilson <jrtilson@gmail.com>
2011-02-07 20:54:32 +0000

    Script to fix a broken poll

---
+++ lib/fix_polls.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once(dirname(dirname(dirname(dirname(__FILE__)))) . "/engine/start.php");
+
+global $CONFIG;
+
+
+$guid = get_input('guid');
+
+$poll = get_entity($guid);
+
+$options = unserialize($poll->poll_content);
+
+// Get annotations
+$annotations = get_annotations($poll->getGUID(), "object", "poll", "", "", 0, 99999);
+
+
+$new_options = array();
+foreach ($options as $key => $option) {
+	$new_options[$key+1] =$option;
+}
+
+$poll->poll_content = serialize($new_options);
+$poll->save();
+
+foreach($annotations as $vote) {
+	
+	$name = (int)$vote->name + 1;
+	$value = (int)$vote->value +1;
+	echo "Name: " . $vote->name . "<br/>Value: " . $vote->value . "</br>";
+	
+	create_annotation($poll->getGUID(), $name, $value, $vote->value_type, $vote->owner_guid, $vote->access_id);
+	delete_annotation($vote->id);
+}
+
+print_r_html($annotations);
+
+
+?>
